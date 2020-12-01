42224546b7691a72c030e35581ac54b5ffc5f827
Denis Capece <deniscapece@hotmail.com>
2020-12-01 23:20:46 +0100

    feat(view + css) login + signup + menu + css

---
+++ view/menu.php
@@ -0,0 +1,12 @@
+<div class=menu>
+    <div class=left>
+        <h1>Trello!</h1>
+    </div>
+    <div class=right>
+        <i class="fas fa-user"></i>
+        <!-- <?= $user->fullname ?> -->
+        <a href="user/logout"><i class="fas fa-sign-out-alt"></i></a>
+        <a href="user/login"><i class="fas fa-sign-in-alt"></i></a>
+        <i class="fas fa-user-plus"></i>
+    </div>
+</div>
