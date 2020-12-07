ccdc19453397bdad188708f6deef6d6fbcb8ea8c
Etienne <etienne.carlier@collectifs.net>
2020-12-07 15:15:53 +0100

    changements de noms de certaines variables et usage des getters

--- view/view_board.php
+++ view/view_board.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="icon" type="image/png" href="assets/logo.png" />
-    <title>Boards "<?= $board->title ?>"</title>
+    <title>Boards "<?= $board->get_title() ?>"</title>
     <base href="<?= $web_root ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b5a4564c07.js" crossorigin="anonymous"></script>
@@ -17,23 +17,23 @@
         <article>
             <header>
                 <div class="title">
-                    <h2>Board "<?= $board->title ?>"</h2>
+                    <h2>Board "<?= $board->get_title() ?>"</h2>
                     <ul class="icons">
                         <li>
                             <form class='link' action='board/edit' method='post'>
-                                <input type='text' name='id' value='<?= $board->id ?>' hidden>
+                                <input type='text' name='id' value='<?= $board->get_id() ?>' hidden>
                                 <input type='submit' value="&#xf044" class="fas fa-edit" style="background:none">
                             </form>
                         </li>
                         <li>
                             <form class='link' action='board/delete' method='post'>
-                                <input type='text' name='id' value='<?= $board->id ?>' hidden>
+                                <input type='text' name='id' value='<?= $board->get_id() ?>' hidden>
                                 <input type='submit' value="&#xf2ed" class="far fa-trash-alt" style="background:none">
                             </form>
                         </li>
                     </ul>
                 </div>
-                <p class="credit">Created 'code php time' ago by <strong>'<?= $board->owner->fullname ?>'</strong>. 'code php modified'</p>
+                <p class="credit">Created 'code php time' ago by <strong>'<?= $board->get_owner()->get_fullName() ?>'</strong>. 'code php modified'</p>
             </header>
             <div class="column_display">  
                 <?php include("columns.php"); ?>
