199525c829a8d5a4f53adfc8d647f8b896103ed8
Etienne <etienne.carlier@collectifs.net>
2020-12-21 19:37:01 +0100

    test: premier test de unittest

---
+++ tests/APROPOS.md
@@ -0,0 +1,3 @@
+# À propos des test
+lancer tous les tests (sur Linux) depuis de répertoire prwb_20201_a02:
+`tools/phpunit-9.0.phar --bootstrap autoload.php tests`
---
+++ tests/model/UserTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase {
+    public function testTest(){
+        $this->assertEquals(true, true, "Est égal à lui-même");
+    }
+}
