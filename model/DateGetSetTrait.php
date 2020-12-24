b910f0964632e8dba5eb135f96d260b1dfcc765e
Etienne <etienne.carlier@collectifs.net>
2020-12-24 10:28:50 +0100

    refactor: création du trait DateGetSetTrait

---
+++ model/DateGetSetTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Méthodes relatives au get et set de createdAt & modifiedAt.
+Ce trait existe essentiellement pour éviter la répétition des setters
+*/
+
+trait DateGetSetTrait {
+    public function get_createdAt(): DateTime {
+        return $this->createdAt;
+    }
+
+    public function get_modifiedAt(): DateTime {
+        return $this->modifiedAt;
+    }
+
+    public function set_createdAt(?DateTime $createdAt): void {
+        if ($createdAt == null) {
+            $this->createdAt = new DateTime("now");
+        } else {
+            $this->createdAt = $createdAt;
+        }
+    }
+
+    public function set_modifiedAt(?DateTime $modifiedAt): void {
+        if ($modifiedAt == null) {
+            $this->modifiedAt = new DateTime("now");
+        } else {
+            $this->modifiedAt = $modifiedAt;
+        }
+    }
+
+    public function set_modifiedDate_and_get_sql() {
+        $this->set_modifiedAt(new DateTime("now"));
+        return DBTools::sql_date($this->get_modifiedAt());
+    }
+}
