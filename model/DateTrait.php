f8bca7fdcb86f64c2f7fa9e8b8ebd3e6f3fbd954
Etienne <etienne.carlier@collectifs.net>
2020-12-29 17:59:44 +0100

    refactor: fix modified_intvl et allègement méthode intvl

--- model/DateTrait.php
+++ model/DateTrait.php
@@ -62,52 +62,19 @@ trait DateTrait {
         $laps = "1 second ago";
         if(!is_null($intvl)) {
             if ($intvl->y != 0) {
-                if($intvl->y == 1) {
-                    $laps = "1 year ago";
-                } else {
-                    $laps = $intvl->y . " years ago";
-                }
+                $laps = $intvl->y . " years ago";
             } elseif ($intvl->m != 0) {
-                if($intvl->m == 1) {
-                    $laps = "1 month";
-                } else {
-                    $laps = $intvl->m . " months ago";
-                }
+                $laps = $intvl->m . " months ago";
             } elseif ($intvl->d != 0) {
-                if($intvl->d == 1) {
-                    $laps = "1 day";
-                } else {
-                    $laps = $intvl->d . " days ago";
-                }
+                $laps = $intvl->d . " days ago";
             } elseif ($intvl->h != 0) {
-                if($intvl->h == 1) {
-                    $laps = "1 hour";
-                } else {
-                    $laps = $intvl->h . " hours ago";
-                }
+                $laps = $intvl->h . " hours ago";
             } elseif ($intvl->i != 0) {
-                if($intvl->i == 1) {
-                    $laps = "1 minute";
-                } else {
-                    $laps = $intvl->i . " minutes ago";
-                }
+                $laps = $intvl->i . " minutes ago";
             } elseif ($intvl->s != 0) {
-                if($intvl->s == 1) {
-                    $laps = "1 second";
-                } else {
-                    $laps = $intvl->s . " seconds ago";
-                }
+                $laps = $intvl->s . " seconds ago";
             }
         }
         return $laps;
-    }
-
-    public function laps ($firstDate, $secondDate): string {
-        if ($secondDate->diff($firstDate)->format('Y-m-d H:i:s') == "0-0-0 0:0:0") {
-            return "Modified " . $this->intvl($firstDate, $secondDate);
-        }
-        else {
-            return "Never modified";
-        }
     }
 }
