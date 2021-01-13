caae7245d0c8b2f736e83561d47cd8a4686b3132
Etienne <etienne.carlier@collectifs.net>
2021-01-13 13:41:01 +0100

    tests: Déplacement DB dans tests/tools

--- tests/tools/DB.php
+++ tests/tools/DB.php
@@ -1,16 +1,18 @@
-<?php
+<?php namespace tools;
 
+
+use \PDO;
 
 class DB {
     private ?PDO $pdo;
     
     public function __construct() {
-        $dbtype = Configuration::get("dbtype");
-        $dbhost = Configuration::get("dbhost");
-        $dbuser = Configuration::get("dbuser");
-        $dbpassword = Configuration::get("dbpassword");
+        $dbtype = \Configuration::get("dbtype");
+        $dbhost = \Configuration::get("dbhost");
+        $dbuser = \Configuration::get("dbuser");
+        $dbpassword = \Configuration::get("dbpassword");
 
-        $this->pdo = new PDO("{$dbtype}:host={$dbhost};charset=utf8", $dbuser, $dbpassword);
+        $this->pdo = new \PDO("{$dbtype}:host={$dbhost};charset=utf8", $dbuser, $dbpassword);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
@@ -19,7 +21,7 @@ class DB {
     }
     
     public function init() {
-        $dbname = Configuration::get("dbname");
+        $dbname = \Configuration::get("dbname");
         $sql = file_get_contents("tests/database/tests.sql");
         $query = $this->pdo->prepare($sql);
         $query->execute();
