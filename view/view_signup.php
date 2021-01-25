164b20d73c246a0c98359b2775c7be1c6472127c
Denis Capece <deniscapece@hotmail.com>
2021-01-25 14:05:54 +0100

    fix(signup) retient psw + confirm si erreur

--- view/view_signup.php
+++ view/view_signup.php
@@ -31,11 +31,11 @@
                     </li>
                     <li class="form-row">
                         <label for="password"><i class="fas fa-lock"></i></label>
-                        <input type="password" name="password" placeholder="Password">
+                        <input type="password" name="password" placeholder="Password" value="<?= $password ?>">
                     </li>
                     <li class="form-row">
                         <label for="confirm"><i class="fas fa-lock"></i></label>
-                        <input type="password" name="confirm" placeholder="Confirm your password">
+                        <input type="password" name="confirm" placeholder="Confirm your password" value="<?= $confirm ?>">
                     </li>
                     <li class="form-row">
                         <input type="submit" value="Sign Up">
