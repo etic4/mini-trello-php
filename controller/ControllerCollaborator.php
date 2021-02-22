9159b04053d41619eafe3b40c0915f2fcd557ea8
Etienne <etienne.carlier@collectifs.net>
2021-02-22 15:14:20 +0100

    feat(collaborators): ajout fichier controller

---
+++ controller/ControllerCollaborator.php
@@ -0,0 +1,17 @@
+<?php
+require_once "framework/Controller.php";
+
+class ControllerCollaborator extends Controller {
+
+    public function index() {
+        // TODO: Implement index() method.
+    }
+
+    public function add() {
+
+    }
+
+    public function remove() {
+
+    }
+}
