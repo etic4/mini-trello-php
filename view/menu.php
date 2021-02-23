e3ff954e4bdf69f5a7c413f9fb5e2923072b4d82
Etienne <etienne.carlier@collectifs.net>
2021-02-23 14:26:11 +0100

    feat(user manager): copie fichiers Denis

--- view/menu.php
+++ view/menu.php
@@ -5,7 +5,12 @@
     <div class="right">
         <?php if (isset($user) && $user instanceof User): ?>
             <?= CtrlTools::breadcrumb(); ?>
-            <p id="logged-user"><i class="fas fa-user"></i><?= $user->get_fullName() ?></p>
+            <?php if($user->is_admin()): ?> 
+                <p><a href="user/manage">Manage users</a></p>
+                <p id="logged-user"><i class="fas fa-user-shield"></i><?= $user->get_fullName() ?></p>
+            <?php else: ?>
+                <p id="logged-user"><i class="fas fa-user"></i><?= $user->get_fullName() ?></p>
+            <?php endif ?>
             <p><a href="user/logout"><i class="fas fa-sign-out-alt"></i></a></p>
         <?php else: ?>
             <p><a class="loginLink" href="user/login"><i class="fas fa-sign-in-alt"></i></a></p>
