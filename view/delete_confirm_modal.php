f02cb6650ea6e47f172efb836679e9e828a6e827
Etienne <etienne.carlier@collectifs.net>
2021-05-09 21:24:56 +0200

    feat(confirm & modal): fonctionnels

---
+++ view/delete_confirm_modal.php
@@ -0,0 +1,9 @@
+<div id="delete-confirm" class="has-text-danger has-text-centered display-none">
+    <div class="icon is-very-large mt-4">
+        <i class="fas fa-trash-alt fa-4x"></i>
+    </div>
+    <div class="mt-6">
+        <p class="mb-2">Do you really want to delete this <span id="delete-class-name"></span> ?</p>
+        <p class="">This process cannot be undone.</p>
+    </div>
+</div>
